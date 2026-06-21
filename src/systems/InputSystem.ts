010efe2b39764ac4f42d9475bc546c763649299c
2452327415qh-debug <2452327415qh@gmail.com>
2026-06-21 08:24:03 +0800

    修复鼠标点击事件绑定，确保仅在点击画布时设置指针锁定

--- src/systems/InputSystem.ts
+++ src/systems/InputSystem.ts
@@ -27,7 +27,9 @@ export class InputSystem {
     private boundKeydown = (e: KeyboardEvent) => this.onKeydown(e); // 键盘按下绑定
     private boundKeyup = (e: KeyboardEvent) => this.onKeyup(e); // 键盘抬起绑定
     private boundMouseMove = (e: MouseEvent) => this.onMouseMove(e); // 鼠标移动绑定
-    private boundMouseClick = () => this.ctrl.cam.setPointerLock(); // 鼠标点击绑定
+    private boundMouseClick = (e: MouseEvent) => {
+        if (e.target === this.ctrl.viewer.canvas) this.ctrl.cam.setPointerLock(); // 鼠标点击绑定
+    };
     private boundBlur = () => this.resetKeys(); // 页面失焦时重置按键状态
 
     private dragging = false; // 是否正在拖拽
